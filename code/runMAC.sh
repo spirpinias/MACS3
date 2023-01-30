39e27e6d9e10ec812d8b45cb3fafe5d243974fce
Stephen Pirpinias <stephen@codeocean.com>
2023-01-30 16:11:23 +0000

    Edited runMAC.sh

--- code/runMAC.sh
+++ code/runMAC.sh
@@ -32,8 +32,8 @@ then
             control_file=$(find -L ../data -name "*.bam" | grep -i ''${control}'')
             case_file=$(find -L ../data -name "*.bam" | grep -i ''${case}'')
 
-            control_count=$(find -L ../data -name "*.bam" | grep -ci ''${control}'')
-            case_count=$(find -L ../data -name "*.bam" | grep -ci ''${case}'')
+            control_count=$(echo $control_file | wc -l)
+            case_count=$(echo $case_file | wc -l)
 
             echo "Number of Control Files Found : ${control_count}"
             echo "Number of Case Files Found : ${case_count}" 
