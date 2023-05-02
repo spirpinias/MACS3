5fe0833ccac6886acfb7677d00106d9fa3bcda6a
Stephen Pirpinias <stephen@codeocean.com>
2023-05-02 16:28:46 +0000

    Edited config.sh

--- code/config.sh
+++ code/config.sh
@@ -43,6 +43,7 @@ if [ "${4}" = "True" ]; then
     then
         delim_compare_count=$(cat $compare_sheet | grep -c ",")
     else
+        compare_sheet_count=0
         echo "Only 1 Compare Sheet Allowed!"
     fi
 else
