0bdeb22f3366484fd61bb5b0e6736a3cec61d02e
Stephen Pirpinias <stephen@codeocean.com>
2023-05-08 13:33:59 +0000

    Edited config.sh

--- code/config.sh
+++ code/config.sh
@@ -14,7 +14,6 @@ fi
 bam_files=$(find -L ../data -name "*.bed*" -o -name "*.bam" -o -name "*.bam.gz" -o -name "*.bed.gz")
 bam_count=$(echo $bam_files | wc -w)
 
-
 # Compare Sheet
 compare_sheet=$(find -L ../data -name "*.csv")
 compare_sheet_count=$(echo $compare_sheet | wc -w)
